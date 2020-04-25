cafabd9347eed00a43e61858d2ef3841da8aa209
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2020-04-25 23:09:05 +0300

    faster crop_white

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ import numpy as np
 def crop_white(image: np.ndarray) -> np.ndarray:
     assert image.shape[2] == 3
     assert image.dtype == np.uint8
-    ys, xs, _ = (image != 255).nonzero()
+    ys, = (image.min((1, 2)) != 255).nonzero()
+    xs, = (image.min(0).min(1) != 255).nonzero()
     if len(xs) == 0 or len(ys) == 0:
         return image
-    return image[ys.min():ys.max(), xs.min():xs.max()]
+    return image[ys.min():ys.max() + 1, xs.min():xs.max() + 1]
