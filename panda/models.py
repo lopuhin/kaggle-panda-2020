c9913ac9e42a69685cc3c947c03be15c5c012acd
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2020-05-09 21:51:22 +0300

    Fix resnet_timm

--- panda/models.py
+++ panda/models.py
@@ -146,11 +146,27 @@ resnet50_swsl = partial(resnet_swsl, name='resnet50_swsl')
 resnext50_32x4_swsl = partial(resnet_swsl, name='resnext50_32x4d_swsl')
 
 
+class ResNetTimm(ResNet):
+    def get_features(self, x):
+        base = self.base
+        x = base.conv1(x)
+        x = base.bn1(x)
+        x = base.act1(x)
+        x = base.maxpool(x)
+
+        x = base.layer1(x)
+        x = base.layer2(x)
+        x = base.layer3(x)
+        x = base.layer4(x)
+
+        return x
+
+
 def resnet_timm(name: str, head_name: str, pretrained: bool = True):
     import timm
     base = timm.create_model(name, pretrained=pretrained)
     head_cls = globals()[head_name]
-    return ResNet(base=base, head_cls=head_cls)
+    return ResNetTimm(base=base, head_cls=head_cls)
 
 
 resnet34_timm = partial(resnet_timm, name='resnet34')
