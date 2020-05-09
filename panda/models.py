495003820acd0586596848cef7d26c03e2fdfa23
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2020-05-09 21:45:39 +0300

    rm fixup: it's slower and uses more memory

--- panda/models.py
+++ panda/models.py
@@ -6,7 +6,7 @@ from torch import nn
 from torch.nn import functional as F
 import torchvision.models
 
-from . import gnws_resnet, fixup_resnet
+from . import gnws_resnet
 
 
 class ResNet(nn.Module):
@@ -154,27 +154,3 @@ def resnet_timm(name: str, head_name: str, pretrained: bool = True):
 
 
 resnet34_timm = partial(resnet_timm, name='resnet34')
-
-
-class ResNetFixUp(ResNet):
-    def get_features(self, x):
-        base = self.base
-        x = base.conv1(x)
-        x = base.relu(x + base.bias1)
-        x = base.maxpool(x)
-
-        x = base.layer1(x)
-        x = base.layer2(x)
-        x = base.layer3(x)
-        x = base.layer4(x)
-
-        return x
-
-
-def resnet_fixup(name: str, head_name: str, pretrained: bool = True):
-    base = getattr(fixup_resnet, name)()
-    head_cls = globals()[head_name]
-    return ResNetFixUp(base=base, head_cls=head_cls)
-
-
-resnet34_fixup = partial(resnet_fixup, name='fixup_resnet34')
